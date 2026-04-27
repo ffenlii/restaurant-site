6e26f7cad8aba83e45d2ea20c72d34e2696d3823
ffenlii <nikashvets2007@gmain.com>
2026-04-27 14:22:38 +0300

    Finished writing the adaptive graphics for the main page

---
+++ js/_energy-slider.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+  // Використовуємо ваш клас .swiper
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true, // Курсор-рука для зручності
+
+  // Налаштування адаптиву
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+
+  // Налаштування крапок під слайдером
+  pagination: {
+    el: '.pagination',
+    clickable: true,
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+});
